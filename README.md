94435be5db1e6ded086e06b6ed70c9cc3340abf4
fomoton <fomoton@proton.me>
2025-01-08 16:48:44 -0500

    add better docs

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# cracked-devs
+# Cracked Devs API
+
+A community-driven API showcasing cracked developers who are building in public.
+
+## Requirements
+
+1. You must be building in public (one of the following)
+    - Streaming your development process on platforms like X, Twitch,YouTube, etc.
+    - Posting regular updates on social media about your process
+    - Commiting to a public open source repository
+
+## How to Add Yourself
+
+1. Fork this repository
+2. Add your project details to `data/projects.ts`
+3. Add your developer profile to `data/developers.ts`
+4. Test your changes locally by running `npm run build`
+5. Open a PR back to the main repository
+
+## Guidelines
+
+1. Use your twitter profile picture as your image
+    - Click twitter profile picture
+    - Right click and select "Copy image address"
+2. Keep descriptions concise and professional
+3. Ensure all links are valid
+4. Use unique IDs for both projects and developer profiles (iterate by 1 from the last ID)
+5. Make not of which fields are optional and which are required
--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,13 @@
+/**
+ * Developer type represents a developer profile in the Cracked Devs API
+ * @property id - Unique identifier for the developer
+ * @property name - Name of the developer
+ * @property image - URL to developer's profile image (right click copy image address on your profile picture)
+ * @property github - GitHub username of the developer
+ * @property twitter - Twitter username of the developer
+ * @property description - Brief bio or description of the developer
+ * @property projects - Optional array of projects associated with the developer
+ */
 export type Developer = {
   id: string;
   name: string;
@@ -8,6 +18,16 @@ export type Developer = {
   projects?: Project[];
 };
 
+/**
+ * Project type represents a project in the Cracked Devs API
+ * @property id - Unique identifier for the project
+ * @property name - Display name of the project
+ * @property description - Brief description of what the project does
+ * @property tokenAddress - Optional token address if project has an associated token
+ * @property keyword - Optional keyword/tag to categorize the project (this helps us identify users on social talking about your project)
+ * @property github - Optional GitHub repository URL for the project
+ * @property twitter - Optional Twitter URL for the project
+ */
 export type Project = {
   id: string;
   name: string;
