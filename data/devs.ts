dd10a9a3bf7a6a62a4af147e6ada5537d56e97c0
0xbeyond <0x.beyond.brand@gmail.com>
2025-01-09 15:12:20 -0800

    add oliver to cracked devs

--- data/devs.ts
+++ data/devs.ts
@@ -100,5 +100,14 @@ export const developers: Developer[] = [
         twitter: 'ineedtendies',
         description: '',
         projects: [projects.Degen8Ball, projects.Eliza]
+      },
+      {
+        id: '12',
+        name: 'Oliver',
+        image: 'https://pbs.twimg.com/profile_images/1870133192201576448/4y3f69Hg_400x400.jpg',
+        github: '0xbeyond',
+        twitter: '0xOlim',
+        description: '',
+        projects: [projects.FOMO]
       }
 ];
