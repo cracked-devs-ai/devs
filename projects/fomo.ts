aad3d74b8ae1cb9ba9dc7bb5448eabd04cb1ddd4
fomoton <fomoton@proton.me>
2025-01-14 17:00:10 -0500

    kickoff gh action

--- projects/fomo.ts
+++ projects/fomo.ts
@@ -2,7 +2,7 @@ import { Project } from '../types/types';
 
 export const fomo: Project = {
     name: "FOMO",
-    description: "Launch your own AI agent in seconds with FOMO. No code required. Just a few clicks and you're ready to go.",
+    description: "Launch your own AI agent in seconds with FOMO. No code required. Just a few clicks and you're ready to go!",
     tokenAddress: "ZxBon4vcf3DVcrt63fJU52ywYm9BKZC6YuXDhb3fomo",
     github: "https://github.com/FomoProtocol",
 };
