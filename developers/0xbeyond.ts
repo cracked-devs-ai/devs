46324abdf65871569054530178d00054b0837e4e
0xbeyond <0x.beyond.brand@gmail.com>
2025-01-15 13:44:28 -0800

    add Oliver to cracked devs

---
+++ developers/0xbeyond.ts
@@ -0,0 +1,11 @@
+import { Developer } from '../types/types';
+import { fomo } from '../projects/fomo';
+
+export const developer: Developer = {
+  name: 'Oliver',
+  image: 'https://pbs.twimg.com/profile_images/1870133192201576448/4y3f69Hg_400x400.jpg',
+  github: '0xbeyond',
+  twitter: '0xOlim',
+  description: '',
+  projects: [fomo],
+};
