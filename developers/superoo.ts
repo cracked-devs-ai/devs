2453b049cc9af75db2346d8d799782d63d4108d0
0xNerd <thejoker3077@gmail.com>
2025-01-14 19:23:57 -0500

    add developer

---
+++ developers/superoo.ts
@@ -0,0 +1,11 @@
+import { Developer } from '../types/types';
+import { chromia } from '../projects/chromia';
+
+export const developer: Developer = {
+  name: "superoo7",
+  image: "https://pbs.twimg.com/profile_images/1850817322812080128/PH3WdtkU_400x400.jpg",
+  github: "superoo7",
+  twitter: "jlwhoo7",
+  description: "Johnson Lai | Head of Data & AI @Chromia | Previously @ChasmNetwork @CoinGecko | Threadoooor & Contributoooor in Crypto x AI!",
+  projects: [chromia]
+};
---
+++ projects/chromia.ts
@@ -0,0 +1,9 @@
+import { Project } from '../types/types';
+
+export const chromia: Project = {
+    name: "Chromia",
+    description: "Chromia is a public #blockchain platform powered by #Rell, built with both developers and end-users in mind.",
+    twitter: "https://x.com/Chromia",
+    website: "https://chromia.com/",
+    keyword: "chromia"
+};
