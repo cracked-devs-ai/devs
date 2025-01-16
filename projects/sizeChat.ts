fafd6816c1dbf835130ef466d06bbde9b5f6030c
crush0x <128001385+crush0x@users.noreply.github.com>
2025-01-17 10:34:39 +1100

    added sizechat & crush0x

---
+++ projects/sizeChat.ts
@@ -0,0 +1,11 @@
+import { Project } from '../types/types';
+
+export const sizeChat: Project = {
+    name: "SizeChat",
+    description: "Instant token-gated chat for any Solana token", 
+    github: "https://github.com/crush0x",
+    tokenAddress: "6cSY6ZXZ2im8DR2q7opgYnU3xZKStJe8z5mzG9twpump",
+    twitter: "https://x.com/sizechat",
+    website: "https://size.chat",
+    keyword: "sizechat"
+};
